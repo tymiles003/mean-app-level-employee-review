dcf99c3a031baa9a3fb9dc0fb048f2f6c9a67541
Bill Lorand <bill.lorand@level-studios.com>
2016-07-28 14:21:59 -0700

    initial version of saving angular form data to

--- public/controllers/register-controller.js
+++ public/controllers/register-controller.js
@@ -2,8 +2,13 @@
 
 angular.module('sugarsnaps').controller('RegisterCtrl', ['$scope', '$http',
 	function($scope, $http) {
+
+		$scope.formInfo = {};
+
 		$scope.register = function() {
+			//alert('You hit register');
 			console.log('You hit register!');
+			console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
 			location.assign("http://localhost:3000/#/dash-test");
 		};
 	}
