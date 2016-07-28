09608d5ee0392b0c5a3dc87349109ec5cf759b1e
scott kao <scott_kao@apple.com>
2016-07-28 09:55:44 -0700

    initial stage of implementing angular form validation. commiting to merge latest update

--- public/controllers/register-controller.js
+++ public/controllers/register-controller.js
@@ -3,6 +3,9 @@
 angular.module('sugarsnaps').controller('RegisterCtrl', ['$scope', '$http',
 	function($scope, $http) {
 		$scope.register = function() {
+			if($scope.userFrom.$valid) {
+				alert('form validated');
+			}
 		};
 	}
 ]);
