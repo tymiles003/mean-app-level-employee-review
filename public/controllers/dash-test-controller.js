fab6fa961ccdc35ae795c7dc2905396cb32c8cad
scott kao <scott_kao@apple.com>
2016-07-29 15:39:11 -0700

    cleaned up code for form validation and exploring angular model to storing user data

--- public/controllers/dash-test-controller.js
+++ public/controllers/dash-test-controller.js
@@ -3,7 +3,5 @@
 angular.module('sugarsnaps').controller('DashCtrl', ['$scope', '$http',
 	function($scope, $http) {
 		$scope.name = 'scott';
-		$scope.register = function() {
-		};
 	}
 ]);
--- public/controllers/register-controller.js
+++ public/controllers/register-controller.js
@@ -6,10 +6,10 @@ angular.module('sugarsnaps').controller('RegisterCtrl', ['$scope', '$http',
 		$scope.formInfo = {};
 
 		$scope.register = function() {
-			if($scope.userFrom.$valid) {
-				console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
-				location.assign("http://localhost:3000/#/dash-test");
-			}
+			console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
+			console.log(JSON.stringify($scope.formInfo.firstname));
+			$scope.firstName = JSON.stringify($scope.formInfo.firstname);
+			location.assign("http://localhost:3000/#/dash-test");
 
 		};
 	}
