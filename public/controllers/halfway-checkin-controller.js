e36d76e6cdfe6bea5ab13b8a9349a756d2cd9eff
Bill Lorand <bill.lorand@level-studios.com>
2016-08-03 13:55:50 -0700

    new skeleton view and controller for halfway checkin form

---
+++ public/controllers/halfway-checkin-controller.js
@@ -0,0 +1,9 @@
+(function(){
+
+	'use strict';
+
+	angular.module(mainAppModuleName).controller('HalfwayCheckinCtrl', [ '$scope', '$http',
+		function($scope, $http){
+			$scope.name = "BillyBob";
+		}]);
+})();
