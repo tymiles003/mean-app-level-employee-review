bf85e3ae7d275acfd3b757dd130e91144c8bd72e
Megan Prior-Pfeifer <mprior@apple.com>
2016-08-04 18:04:53 -0700

    Code cleanup for ninebox and inout functionality. Thanks everyone

---
+++ public/static/js/ninebox.js
@@ -0,0 +1,24 @@
+// ninebox javascript
+// Add active class to parent .rating when input is checked
+
+var els = document.querySelectorAll(".rating-label");
+
+els.forEach(function(item){
+	item.addEventListener("click", doSomething, false);
+})
+
+function doSomething(e) {
+
+	for (var i = 0; i < els.length; i++ ) {
+		console.log(els[i].classList);
+
+		if (els[i].classList.contains("checked")) {
+			els[i].classList.remove("checked");
+		}
+	}
+
+	
+	e.currentTarget.classList.add("checked");
+
+
+}
