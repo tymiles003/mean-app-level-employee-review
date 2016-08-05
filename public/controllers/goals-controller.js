64b448aa09b82fc169c1d2a9e85d85307d7040f9
scott kao <scott_kao@apple.com>
2016-08-05 11:31:45 -0700

    added ng model in none-required fields to save user data

--- public/controllers/goals-controller.js
+++ public/controllers/goals-controller.js
@@ -2,16 +2,18 @@
 
 angular.module('sugarsnaps').controller('GoalsCtrl', ['$scope',
 	function($scope) {
-		console.log('goals controller here');
+
+		$scope.userGoals = {};
 
 		$scope.submit = function() {
 			// console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
 			// $scope.firstName = JSON.stringify($scope.formInfo.firstname);
 
-			if($scope.goalSetting.$invalid) {
+			if($scope.userGoals.$invalid) {
 				console.log('Please fill out the required fields');
 				return;
 			}else {
+				console.log('regForm: ', JSON.stringify($scope.userGoals));
 				location.assign("http://localhost:3000/#/dash-test");
 			}
 
