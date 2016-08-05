9e538bff00c9c79b99ee18584cbf170b6684a78f
scott kao <scott_kao@apple.com>
2016-08-04 17:36:45 -0700

    finished addeding form validation for required field in goal setting form

--- public/controllers/goals-controller.js
+++ public/controllers/goals-controller.js
@@ -1,7 +1,20 @@
 'use strict'
 
-angular.module('sugarsnaps').controller('GoalsCtrl', [
-	function() {
-		console.log('goals controller here')
+angular.module('sugarsnaps').controller('GoalsCtrl', ['scope',
+	function($scope) {
+		console.log('goals controller here');
+
+		// $scope.submit = function() {
+		// 	// console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
+		// 	// $scope.firstName = JSON.stringify($scope.formInfo.firstname);
+
+		// 	if($scope.goalSetting.$invalid) {
+		// 		console.log('Please fill out the required fields');
+		// 		return;
+		// 	}else {
+		// 		location.assign("http://localhost:3000/#/dash-test");
+		// 	}
+
+		// };
 	}
 ]);
