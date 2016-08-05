7339e26afca63d620b7a8b946d6aebf072e8ff54
scott kao <scott_kao@apple.com>
2016-08-04 17:38:24 -0700

    added controller func to goal setting page. stop user from submitting if form is empty

--- public/controllers/goals-controller.js
+++ public/controllers/goals-controller.js
@@ -1,20 +1,20 @@
 'use strict'
 
-angular.module('sugarsnaps').controller('GoalsCtrl', ['scope',
+angular.module('sugarsnaps').controller('GoalsCtrl', ['$scope',
 	function($scope) {
 		console.log('goals controller here');
 
-		// $scope.submit = function() {
-		// 	// console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
-		// 	// $scope.firstName = JSON.stringify($scope.formInfo.firstname);
+		$scope.submit = function() {
+			// console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
+			// $scope.firstName = JSON.stringify($scope.formInfo.firstname);
 
-		// 	if($scope.goalSetting.$invalid) {
-		// 		console.log('Please fill out the required fields');
-		// 		return;
-		// 	}else {
-		// 		location.assign("http://localhost:3000/#/dash-test");
-		// 	}
+			if($scope.goalSetting.$invalid) {
+				console.log('Please fill out the required fields');
+				return;
+			}else {
+				location.assign("http://localhost:3000/#/dash-test");
+			}
 
-		// };
+		};
 	}
 ]);
