78b5e0c500f24a2eb1d5a1ac59e9e939d9d29428
scott kao <scott_kao@apple.com>
2016-08-05 13:28:38 -0700

    fixed broken bug

--- public/controllers/goals-controller.js
+++ public/controllers/goals-controller.js
@@ -6,17 +6,14 @@ angular.module('sugarsnaps').controller('GoalsCtrl', ['$scope',
 		$scope.userGoals = {};
 
 		$scope.submit = function() {
-			// console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
-			// $scope.firstName = JSON.stringify($scope.formInfo.firstname);
 
-			if($scope.userGoals.$invalid) {
+			if($scope.goalSetting.$invalid) {
 				console.log('Please fill out the required fields');
 				return;
 			}else {
 				console.log('regForm: ', JSON.stringify($scope.userGoals));
 				location.assign("http://localhost:3000/#/dash-test");
 			}
-
 		};
 	}
 ]);
