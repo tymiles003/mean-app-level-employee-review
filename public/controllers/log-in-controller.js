d52430259806c0ebdca8b5c4cbb244fd6bf5bb7e
scott kao <scott_kao@apple.com>
2016-08-08 15:12:36 -0700

    added func to log user info in log in and register view. also wrapped ctrl func in IIFF

--- public/controllers/log-in-controller.js
+++ public/controllers/log-in-controller.js
@@ -1,14 +1,34 @@
-'use strict';
+(function(){
+	'user strict';
 
-angular.module('sugarsnaps').controller('LogInCtrl', ['$scope',
-	function($scope, UserInfoService) {
-		$scope.logIn = function() {
-			if($scope.logInForm.$invalid) {
-				console.log('Please fill out the required fields');
-				return;
-			}else {
-				location.assign("http://localhost:3000/#/dashboard");
+	angular.module(mainAppModuleName).controller('LogInCtrl', ['$scope', '$http',
+		function($scope, $http) {
+			$scope.credentials = {};
+			console.log('hello from log in ctrl');
+
+			$scope.logIn = function() {
+				if($scope.logInForm.$invalid) {
+					console.log('Please fill out the required fields');
+					return;
+				} else {
+					console.log('Here is the form info: ' + JSON.stringify($scope.credentials));
+					location.assign("http://localhost:3000/#/dashboard");
+				}
 			}
-		};
-	}
-]);
+		}
+	]);
+})();
+// 'use strict';
+
+// angular.module('sugarsnaps').controller('LogInCtrl', ['$scope',
+// 	function($scope, UserInfoService) {
+// 		$scope.logIn = function() {
+// 			if($scope.logInForm.$invalid) {
+// 				console.log('Please fill out the required fields');
+// 				return;
+// 			}else {
+// 				location.assign("http://localhost:3000/#/dashboard");
+// 			}
+// 		};
+// 	}
+// ]);
--- public/controllers/register-controller.js
+++ public/controllers/register-controller.js
@@ -1,21 +1,22 @@
-'use strict';
+(function() {
 
-angular.module('sugarsnaps').controller('RegisterCtrl', ['$scope', '$http',
-	function($scope, $http) {
+	'use strict';
 
-		$scope.formInfo = {};
+	angular.module(mainAppModuleName).controller('RegisterCtrl', ['$scope', '$http',
+		function($scope, $http) {
+			$scope.formInfo = {}
 
-		$scope.register = function() {
-			// console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
-			// $scope.firstName = JSON.stringify($scope.formInfo.firstname);
+			$scope.register = function(){
+				$scope.firstName = JSON.stringify($scope.formInfo.firstname);
 
-			if($scope.regForm.$invalid) {
-				console.log('Please fill out the required fields');
-				return;
-			}else {
-				location.assign("http://localhost:3000/#/dashboard");
+				if($scope.regForm.$invalid) {
+					console.log('Please fill out the required fields');
+					return;
+				}else {
+					console.log('Here is the form info: ' + JSON.stringify($scope.formInfo));
+					location.assign("http://localhost:3000/#/dashboard");
+				}
 			}
-
-		};
-	}
-]);
+		}
+	]);
+})();
