02765cbdcd86d84ce05445c013a692cf7d559b66
Bill Lorand <bill.lorand@level-studios.com>
2016-08-09 14:38:34 -0700

    data try

--- app/controllers/halfway-server-controller.js
+++ app/controllers/halfway-server-controller.js
@@ -10,17 +10,16 @@
 	// processes the request, save data to mongo db, issue a response (return back the data too?)
 	exports.postData = function(req, res, next) {
 
-		console.log('YES! Server postData Function called!');
-
 		// 	var user = new User(req.body); // from users-server-controller.js
 		var halfway = new Halfway(req.body);
+		console.log('Hey! heres the halfway object', halfway);
 
-		console.log('heres the halfway object', halfway);
+		// try saving the data to the db
+		halfway.save( function(err) {
+			if (err) return console.error('theres an error', err);
+		});
 
-		res.send('hi from server side'); // test to send something back to the client (here, a string)
+		//res.send('hi from server side'); // gotta send something back to the client (here, a string)
 	};
-
-	// MONGOOSE
-	// data.save();//
 
 })();
