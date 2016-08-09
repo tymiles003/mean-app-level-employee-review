f9cfe0a3cc0a08585b55f6295334588b37a91461
scott kao <scott_kao@apple.com>
2016-08-09 16:55:58 -0700

    wrapped ctrl js in IIFF

--- public/controllers/log-in-controller.js
+++ public/controllers/log-in-controller.js
@@ -6,18 +6,16 @@
 		function($scope, $http, $location) {
 			$scope.credentials = {};
 
-			console.log('hello from log in ctrl');
-
 			$scope.logIn = function() {
-					console.log('Here is the credentials: ' + JSON.stringify($scope.credentials));
-					$location.path('/dashboard');
-					// $http.post('#', $scope.credentials)
-					// 	.then(
-					// 		// success callback
-					// 		function(response) {
-					// 			$location.path('/dashboard');
-					// 		}
-					// 	)
+				console.log('Here is the credentials: ' + JSON.stringify($scope.credentials));
+				$location.path('/dashboard');
+				// $http.post('#', $scope.credentials)
+				// 	.then(
+				// 		// success callback
+				// 		function(response) {
+				// 			$location.path('/dashboard');
+				// 		}
+				// 	)
 				// }
 			}
 		}
