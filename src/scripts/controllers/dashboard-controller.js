9c829a3e702e8f04af3ce2bfa110ebdfa4b11b69
Bill Lorand <bill.lorand@level-studios.com>
2016-12-08 15:10:55 -0800

    added angular uib alerts to the dashboard

--- src/scripts/controllers/dashboard-controller.js
+++ src/scripts/controllers/dashboard-controller.js
@@ -2,6 +2,7 @@
 	'use strict';
 
 	var DashboardCtrl = function($scope) {
+
 		$scope.name = 'Scott Prior-Pfeifer III';
 
 		var today = new Date();
@@ -9,6 +10,21 @@
 		$scope.dd = today.getDate();
 		$scope.mm = monthArray[today.getMonth()];
 		$scope.yyyy = today.getFullYear();
+
+
+		// Alerts for Dashboard
+		$scope.alerts = [
+			{ type: 'success', icon: 'ok', msg: 'Hooray! You did it from js!' },
+			{ type: 'info', icon: 'flag', msg: 'Hey, you should know this...from js' },
+			{ type: 'warning', icon: 'exclamation-sign', msg: 'Uh-oh, you better be careful...from js' },
+			{ type: 'danger', icon: 'fire', msg: 'HALP! HALP! Your website is on fire!!! from js' },
+			{ type: 'success', icon: 'ok', msg: 'Hip hop Hooray! - SS Dashboard now includes UIB Dismissible Alerts!' },
+		];
+
+		$scope.closeAlert = function(index) {
+			$scope.alerts.splice(index, 1);
+		};
+
 	};
 
 	module.exports = DashboardCtrl;
