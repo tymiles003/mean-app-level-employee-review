62594afd3b7f9dd8024d9e4ff8a3a755991eaf2e
Bill Lorand <bill_lorand@apple.com>
2017-03-09 13:37:20 -0800

    switched to use ui-sref for linking to SPA states over using href to link to urls ++ moved to a shared include for dashboard-nav.html...and some content tweaks

--- src/scripts/routes/client-routes.js
+++ src/scripts/routes/client-routes.js
@@ -6,50 +6,50 @@
             function($stateProvider, $urlRouterProvider) {
                 $urlRouterProvider.otherwise('/login');
                 $stateProvider
-                .state('register', {
-                    url: '/register',
-                    templateUrl: 'views/register-view.html',
-                    controller: 'RegCtrl'
-                })
-                .state('login', {
-                    url: '/login',
-                    templateUrl: '/views/login-view.html',
-                    controller: 'LogInCtrl'
-                })
-                .state('dashboard', {
-                    url: '/dashboard',
-                    templateUrl: 'views/dashboard-view.html',
-                    controller: 'DashboardCtrl'
-                })
-                .state('resources', {
-                    url: '/dashboard/resources',
-                    templateUrl: 'views/dashboard-resources-view.html',
+                    .state('login', {
+                        url: '/login',
+                        templateUrl: '/views/login-view.html',
+                        controller: 'LogInCtrl'
+                    })
+                    .state('register', {
+                        url: '/register',
+                        templateUrl: 'views/register-view.html',
+                        controller: 'RegCtrl'
+                    })
+                    .state('dashboard', {
+                        url: '/dashboard',
+                        templateUrl: 'views/dashboard-view.html',
+                        controller: 'DashboardCtrl'
+                    })
+                    .state('dash-resources', {
+                        url: '/dashboard/resources',
+                        templateUrl: 'views/dashboard-resources-view.html',
                         // controller: 'DashboardCtrl'
-                })
-                .state('goals', {
-                    url: '/goals',
-                    templateUrl: 'views/goals-view.html',
-                    controller: 'GoalsCtrl'
-                })
-                .state('halfway', {
-                    url: '/reviews/halfway',
-                    templateUrl: 'views/halfway-checkin-view.html',
-                    controller: 'HalfwayCheckinCtrl'
-                })
-                .state('self-assess', {
-                    url: '/reviews/self-assess',
-                    templateUrl: 'views/self-assess-view.html',
-                    // controller: 'selfAssessmentCtrl'
-                })
-                .state('peer-feedback', {
-                    url: '/peer-feedback',
-                    templateUrl: 'views/peer-feedback-view.html',
-                    controller: 'PeerFeedbackCtrl'
-                });
+                    })
+                    .state('goals', {
+                        url: '/goals',
+                        templateUrl: 'views/goals-view.html',
+                        controller: 'GoalsCtrl'
+                    })
+                    .state('peer-feedback', {
+                        url: '/peer-feedback',
+                        templateUrl: 'views/peer-feedback-view.html',
+                        controller: 'PeerFeedbackCtrl'
+                    })
+                    .state('halfway', {
+                        url: '/reviews/halfway',
+                        templateUrl: 'views/halfway-checkin-view.html',
+                        controller: 'HalfwayCheckinCtrl'
+                    })
+                    .state('self-assess', {
+                        url: '/reviews/self-assess',
+                        templateUrl: 'views/self-assess-view.html',
+                        // controller: 'selfAssessmentCtrl'
+                    });
             }
         ]);
-
     };
 
     module.exports = routes;
+
 })(document, window, angular);
