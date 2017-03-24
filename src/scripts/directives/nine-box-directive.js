d343a6099fba233adad7b6228dcfcc37fede0d20
scott kao <scott_kao@apple.com>
2017-03-24 12:57:55 -0700

    SUG67/TASK: added templateUrl ref to the ninebox diagram view

--- src/scripts/directives/nine-box-directive.js
+++ src/scripts/directives/nine-box-directive.js
@@ -4,24 +4,26 @@
     var nineBoxDirective = function() {
         return {
             restrict: 'E',
-            link: link
+            link: link,
+            templateUrl: '/views/shared/nine-box-diagram.html'
         };
         function link(scope, element){
+            console.log('element', element);
+            var nineBoxRating = element[0].append
+            // function checkedBox(e) {
+            //     for (var i = 0; i < rating.length; i++ ) {
+            //         console.log(rating[i].classList);
+            //         if (rating[i].classList.contains("checked")) {
+            //             rating[i].classList.remove("checked");
+            //         }
+            //     }
+            //     e.currentTarget.classList.add("checked");
+            // }
 
-            function checkedBox(e) {
-                for (var i = 0; i < rating.length; i++ ) {
-                    console.log(rating[i].classList);
-                    if (rating[i].classList.contains("checked")) {
-                        rating[i].classList.remove("checked");
-                    }
-                }
-                e.currentTarget.classList.add("checked");
-            }
+            // rating.forEach(function(item){
 
-            rating.forEach(function(item){
-
-                item.addEventListener("click", checkedBox);
-            });
+            //     item.addEventListener("click", checkedBox);
+            // });
         }
     };
 
