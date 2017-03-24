f39434dffed7fc0f4cb74c8f193a84a268b5cfae
Bill Lorand <bill_lorand@apple.com>
2017-03-23 19:39:43 -0700

    SUG-79 added hack respect for logged in users from login to dashboard

--- src/scripts/controllers/login-controller.js
+++ src/scripts/controllers/login-controller.js
@@ -1,11 +1,22 @@
 (function(document, window, angular) {
 	'use strict';
 
-	var LoginCtrl = function($scope, $http, $location) {
-		$scope.credentials = {};
+	var LoginCtrl = function($scope, $http, $location, UserStorageService) {
+
+		$scope.loginFormInfo = {};
 
 		$scope.loginUser = function() {
-			// console.log('Here is the credentials: ' + JSON.stringify($scope.credentials));
+
+			// 0- debug log some stuffs
+			console.log('Here is the loginFormInfo: ' + JSON.stringify($scope.loginFormInfo));
+			console.log('UserStorageService is: ', UserStorageService); // should be an object
+
+			// 1- set the currentUser in the service based on login string (demo hard-coded to skao, mprior, willoran)
+			UserStorageService.currentUser = $scope.loginFormInfo.userLogin;
+
+			console.log('currentUser is now: ', UserStorageService.currentUser);
+
+			// get thee to the dashboard
 			$location.path('/dashboard');
 		};
 	};
